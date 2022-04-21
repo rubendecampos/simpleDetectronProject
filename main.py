1bf532827826f09764e25b537241cd84f2d0c47c
Ruben DECAMPOS <rdcampos@vws099.idiap.ch>
2022-04-21 10:49:42 +0200

    changing main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from statistics import mode
 from Detector import *
 
 # MODFIY THE MODEL TYPE TO GET OTHER RESULTS
-detector = Detector(model_type = "OD")
+detector = Detector(model_type = "IS")
 
 detector.onImage("./images/1.jpg")
