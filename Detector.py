52e7afbf0bfdf50b312d1ade51f0333e307661cf
rubendecampos <ruben_campos@hotmail.ch>
2022-04-21 11:09:41 +0200

    adding detectron2

--- Detector.py
+++ Detector.py
@@ -26,7 +26,7 @@ class Detector:
             self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")
 
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
-        self.cfg.MODEL.DEVICE = "gpu" if torch.cuda.is_available() else "cpu" #cpu or cuda
+        self.cfg.MODEL.DEVICE = "cuda" #if torch.cuda.is_available() else "cpu" #cpu or cuda
 
         self.predictor = DefaultPredictor(self.cfg)
         
